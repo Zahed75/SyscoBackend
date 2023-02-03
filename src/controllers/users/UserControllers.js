8afeba2a8f3a665c688b406ff503256085bdaf68
Zahed75 <zahedhasan.cs@gmail.com>
2023-02-03 23:32:59 +0600

    user controller created

---
+++ src/controllers/users/UserControllers.js
@@ -0,0 +1,11 @@
+const DataModel=require("../../models/users/UserModel");
+const OTPSModel=require("../../models/users/OTPSModel");
+
+const UserCreateService=require("../../services/users/UserCreateService");
+const UserLoginService=require("../../services/users/UserLoginService");
+const UserUpdateService=require("../../services/users/UserUpdateService");
+const UserDetailsService=require("../../services/users/UserDetailsService");
+const UserResetPassService=require("../../services/users/UserResetPassService");
+const UserVerifyOtpService=require("../../services/users/UserVerifyOtpService");
+const UserVerifyEmailService=require("../../services/users/UserVerifyEmailService");
+
