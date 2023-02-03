0e1541746fede5878edac1a1582af83f62fc644a
Zahed75 <zahedhasan.cs@gmail.com>
2023-02-03 20:37:17 +0600

    utility file created

---
+++ src/utility/SendEmailUtility.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer');
+
+const SendEmailUtility = async (EmailTo, EmailText, EmailSubject) => {
+
+    var transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        auth: {
+            user: "tech.syscomatic@gmail.com",
+            pass: "dhmjzcefemgcficw"
+        }
+    });
+
+    let mailOptions = {
+        from: 'Syscomatic-Inventory Management System <tech.syscomatic@gmail.com>',
+        to: EmailTo,
+        subject: EmailSubject,
+        text: EmailText
+    };
+
+    return await transporter.sendMail(mailOptions)
+
+}
+
+//https://nodemailer.com/about/
+module.exports = SendEmailUtility
