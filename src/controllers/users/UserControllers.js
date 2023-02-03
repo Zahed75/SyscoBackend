7644ce294483799078482d8289cd75423e0b0bef
Zahed75 <zahedhasan.cs@gmail.com>
2023-02-03 23:36:21 +0600

    controller route updated

--- src/controllers/users/UserControllers.js
+++ src/controllers/users/UserControllers.js
@@ -1,11 +1,33 @@
-const DataModel=require("../../models/users/UserModel");
-const OTPSModel=require("../../models/users/OTPSModel");
+const DataModel = require("../../models/users/UserModel");
+const OTPSModel = require("../../models/users/OTPSModel");
 
-const UserCreateService=require("../../services/users/UserCreateService");
-const UserLoginService=require("../../services/users/UserLoginService");
-const UserUpdateService=require("../../services/users/UserUpdateService");
-const UserDetailsService=require("../../services/users/UserDetailsService");
-const UserResetPassService=require("../../services/users/UserResetPassService");
-const UserVerifyOtpService=require("../../services/users/UserVerifyOtpService");
-const UserVerifyEmailService=require("../../services/users/UserVerifyEmailService");
+const UserCreateService = require("../../services/users/UserCreateService");
+const UserLoginService = require("../../services/users/UserLoginService");
+const UserUpdateService = require("../../services/users/UserUpdateService");
+const UserDetailsService = require("../../services/users/UserDetailsService");
+const UserResetPassService = require("../../services/users/UserResetPassService");
+const UserVerifyOtpService = require("../../services/users/UserVerifyOtpService");
+const UserVerifyEmailService = require("../../services/users/UserVerifyEmailService");
+
+
+
+// User Registrations
+exports.Registration = async (req, res) => {
+    let Result = await UserCreateService(req, DataModel)
+    res.status(200).json(Result)
+}
+
+//User Login
+
+exports.Login=async (req,res)=>{
+    let Result=await UserLoginService(req,DataModel)
+    res.status(200).json(Result)
+}
+
+
+//User Profile Update
+
+exports.ProfileUpdate=async (req,res)=>{
+
+}
 
