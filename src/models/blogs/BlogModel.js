efcc71f05032824e8311209ad679aca7b57d463e
Zahed75 <zahedhasan.cs@gmail.com>
2023-02-04 13:06:50 +0600

    blog model done

--- src/models/blogs/BlogModel.js
+++ src/models/blogs/BlogModel.js
@@ -1,7 +1,18 @@
 const mongoose=require("mongoose");
+const users=require("../users/UserModel")
 const BlogSchema=mongoose.Schema({
     user:{
-        type:mongoose.Schema.Types.ObjectId,ref:'users',
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'users',
+    },
+
+    blogTitle:{
+        type:String,
+        trim:true,
+        required:[true,'Please enter the blog title'],
+        max:220,
+
     }
+
 
 })
