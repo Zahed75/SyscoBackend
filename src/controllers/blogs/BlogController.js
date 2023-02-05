2d73c2dc088f9640d267e56e0d550ca632e9b1eb
Zahed75 <zahedhasan.cs@gmail.com>
2023-02-05 22:04:33 +0600

    delete api endpoint done

--- src/controllers/blogs/BlogController.js
+++ src/controllers/blogs/BlogController.js
@@ -1,13 +1,11 @@
 const BlogDb = require("../../models/blogs/BlogModel");
 const CreateBlogService = require("../../services/blogs/CreateBlogService");
-const AllBlogService=require("../../services/blogs/AllPostService");
+const AllBlogService = require("../../services/blogs/AllPostService");
 const EditBlogService = require("../../services/blogs/EditBlogService");
-const DeleteBlogService=require("../../services/blogs/DeleteBlogService");
-
+const DeleteBlogService = require("../../services/blogs/DeleteBlogService");
 
 
 // CreateBlog
-
 exports.CreateBlog = async (req, res) => {
     let Result = await CreateBlogService(req, BlogDb)
     res.status(200).json(Result)
@@ -15,22 +13,19 @@ exports.CreateBlog = async (req, res) => {
 
 
 //All Blogs
-exports.AllBlog=async (req,res)=>{
+exports.AllBlog = async (req, res) => {
     let Result = await AllBlogService(req, BlogDb)
     res.status(200).json(Result)
 }
 
 //Edit Blog
-
-exports.BlogUpdate=async (req,res)=>{
+exports.BlogUpdate = async (req, res) => {
     let Result = await EditBlogService(req, BlogDb)
     res.status(200).json(Result)
 }
 
 //Delete Blog
-
-
-exports.BlogDelete=async(req,res)=>{
+exports.BlogDelete = async (req, res) => {
     let Result = await DeleteBlogService(req, BlogDb)
     res.status(200).json(Result)
 }
