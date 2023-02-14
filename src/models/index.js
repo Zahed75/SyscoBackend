7257620408ef478c5c1bd5415018dbac66cc5b19
Zahed75 <zahedhasan.cs@gmail.com>
2023-02-15 01:46:51 +0600

    accordion model update

--- src/models/index.js
+++ src/models/index.js
@@ -2,8 +2,9 @@ const BlogModel = require("../models/blogs/BlogModel");
 const OTPSModel = require("../models/users/OTPSModel");
 const UserModel = require("../models/users/UserModel");
 const brandLogoModel=require("../models/brands/HomeModel");
+const accordionModel=require("../models/brands/AccordionModel");
 
 
 module.exports = {
-    BlogModel, OTPSModel, UserModel,brandLogoModel
+    BlogModel, OTPSModel, UserModel,brandLogoModel,accordionModel
 }
