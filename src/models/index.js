20a227bbee595a87cf9ea35b6e6bcaac1640165e
Zahed75 <zahedhasan.cs@gmail.com>
2023-02-15 04:20:12 +0600

    portfolio model updated

--- src/models/index.js
+++ src/models/index.js
@@ -4,8 +4,9 @@ const UserModel = require("../models/users/UserModel");
 const brandLogoModel=require("../models/brands/HomeModel");
 const accordionModel=require("../models/brands/AccordionModel");
 const ServiceModel=require("../models/brands/ServiceModel");
+const PortfolioModel=require("../models/brands/PortfolioModel");
 
 
 module.exports = {
-    BlogModel, OTPSModel, UserModel,brandLogoModel,accordionModel,ServiceModel
+    BlogModel, OTPSModel, UserModel,brandLogoModel,accordionModel,ServiceModel,PortfolioModel
 }
